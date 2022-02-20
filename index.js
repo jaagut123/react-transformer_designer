afeb9922e0288c9506596086534017fe22df110e
jonel.agutaya <82687486+jaagut123@users.noreply.github.com>
2022-02-20 15:58:06 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+      import React from 'react'
+      import ReactDOM from 'react-dom'
+      
+      function App() {
+        return <>
+          <h1><center>Hello World!</center></h1>
+          <p>
+            Nice! This is great for learning React.js
+          </p>
+          </>
+      }
+      
+      ReactDOM.render(<App />, document.getElementById('root'))
